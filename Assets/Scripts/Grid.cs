47a038c772671c0edb5a4554658d5efd5de73e4b
TyLanger <ty.langer16@gmail.com>
2017-04-02 00:17:58 -0600

    Added gitignore. Made a grid script to handle the grid of the game. Made a few helper functions in it.

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+	Transform[,] grid;
+	int xGridSize;
+	int zGridSize;
+	public float gridSpacing;
+
+	void Start() {
+		grid = new Transform[xGridSize, zGridSize];
+	}
+
+	Vector3 gridToVec3(int xGrid, int zGrid)
+	{
+		return new Vector3 (xGrid * gridSpacing, 0, zGrid * gridSpacing) + transform.position;
+	}
+
+	Transform getTransformAtGrid(int xGrid, int zGrid)
+	{
+		return grid [xGrid, zGrid];
+	}
+
+}
