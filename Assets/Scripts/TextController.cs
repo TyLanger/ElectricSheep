b3b65d073e603c702329c1f081f71935cee5885e
TyLanger <ty.langer16@gmail.com>
2017-04-02 18:47:34 -0600

    Added textController to handle the 3D text effects.

---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextController : MonoBehaviour {
+
+
+	public Transform cameraTrans;
+
+	public TextMesh[] textMeshes;
+	public string message;
+	public float letterSpacing;
+
+	// Use this for initialization
+	void Start () {
+		
+		for (int i = 0; i < message.Length; i++) {
+			var text = Instantiate (textMeshes[i], transform.position + new Vector3(i * letterSpacing, 0, 0), cameraTrans.rotation) as TextMesh;
+			text.text = message.Substring (i, 1);
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
