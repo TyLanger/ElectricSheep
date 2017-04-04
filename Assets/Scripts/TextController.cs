855f6bc344b500682601f9f39c54ee7435fa9937
TyLanger <ty.langer16@gmail.com>
2017-04-04 12:17:47 -0600

    Added a dictionary to store the widths of each character. Changed the default message of the prefab 3D text to be all the characters needed. This needs to be there to fill up the dictionary.

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -11,6 +11,8 @@ public class TextController : MonoBehaviour {
 	public string message;
 	public float letterSpacing;
 	public float heightScale;
+	CharacterInfo[] charInfo;
+	Dictionary<char, int> charWidthDic;
 
 	public AnimationCurve xycurve;
 
@@ -22,6 +24,40 @@ public class TextController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		var keys = xycurve.keys;
+
+		// sorts the message into alphabetical order
+		charInfo = textMesh.font.characterInfo;
+		//Debug.Log ((70 - 'A'));
+
+		charWidthDic = new Dictionary<char, int>();
+
+
+		for (int t = 0; t < charInfo.Length; t++) {
+			//Debug.Log(((char)charInfo[t].index) + " " + charInfo[t].advance + " " + charInfo[t].index);
+			// fill up the dictionary
+			charWidthDic.Add (((char)charInfo [t].index), charInfo [t].advance);
+		}
+
+		/* Dictionary testing
+		int width;
+		if(charWidthDic.TryGetValue('G', out width))
+			Debug.Log ("G: " + width);
+		if(charWidthDic.TryGetValue('o', out width))
+			Debug.Log ("o: " + width);
+		if(charWidthDic.TryGetValue('o', out width))
+			Debug.Log ("o: " + width);
+		if(charWidthDic.TryGetValue('d', out width))
+			Debug.Log ("d: " + width);
+		if(charWidthDic.TryGetValue(' ', out width))
+			Debug.Log (" : " + width);
+		if(charWidthDic.TryGetValue('D', out width))
+			Debug.Log ("D: " + width);
+		if(charWidthDic.TryGetValue('a', out width))
+			Debug.Log ("a: " + width);
+		if(charWidthDic.TryGetValue('y', out width))
+			Debug.Log ("y: " + width);
+		*/
+
 		for (int i = 0; i < message.Length-1; i++) {
 			
 
@@ -38,10 +74,14 @@ public class TextController : MonoBehaviour {
 			Debug.Log ("Theta: " + rot);
 			*/
 
-			
+
 			//text.transform.Rotate (new Vector3 (0,0,rot * ((v2.y - v1.y)/Mathf.Abs(v2.y - v1.y))));
+			//var charInfo = text.font.characterInfo;
 
 			text.text = message.Substring (i, 1);
+			//Debug.Log (text.text + ": " + (text.text.ToCharArray()[0]+0));
+			//charInfo [(char)text.text];
+
 			/* This makes the letters shake kinda.
 			 * Shelving this for now
 			Shake textShake = text.GetComponent<Shake> ();
@@ -55,4 +95,40 @@ public class TextController : MonoBehaviour {
 	void Update () {
 		
 	}
+
+	/*
+	 * Not needed anymore with the dictionary
+	int getCharacterWidth(int unicode)
+	{
+
+		// char array only has some characters in it.
+		// It has what I set as the message in the textMesh
+		// Right now, I have ABC... abc... .,:;'!?(space)
+		// the symbols are at the start of the array
+		// starting at your unicode - 'A'
+		// Should give an offset of 0 for A, 1 for B, etc.
+		// Know there are always 52 letters, the length-52 is the number of symbols
+		// add that on to get closer to the start of the letters(probably exact)
+		// Adding numbers might change this
+		// For symbols smaller than A, they will return a negative so the max function makes it always start at 0
+		// numbers go first
+		// then symbols
+		// Capital
+		// 7 spacees between Z a
+		// lowercase
+		// can't find lowercase
+		for (int i = Mathf.Max(0, (unicode - 'A' + (charInfo.Length-52))); i < charInfo.Length; i++) {
+			Debug.Log ("step");
+			if (charInfo [i].index == unicode) {
+				return charInfo [i].advance;
+			}
+		}
+		return 0;
+	}
+
+	int getCharacterWidth(char character)
+	{
+		return getCharacterWidth (character + 0);
+	}
+	*/
 }
